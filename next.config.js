cbe25ff11f28ef04bdde79f52a658a851ccf95a1
Jatin <jatinpathania1@gmail.com>
2026-03-04 22:53:16 +0530

    updated next.js to 16 with turbopack for project

--- next.config.js
+++ next.config.js
@@ -1,38 +1,39 @@
 const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains; preload',
-          },
-          {
-            key: 'Permissions-Policy',
-            value: 'microphone=(self), camera=(self)',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-        ],
-      },
-    ];
-  },
-  webpack: (config) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    })
-    return config
-  },
+    output: 'standalone',
+    reactStrictMode: true,
+    headers: async () => {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'microphone=(self), camera=(self)',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                ],
+            },
+        ];
+    },
+    turbopack: {},
+    webpack: (config) => {
+        config.externals.push({
+            'utf-8-validate': 'commonjs utf-8-validate',
+            'bufferutil': 'commonjs bufferutil',
+        })
+        return config
+    },
 }
 
 module.exports = nextConfig
